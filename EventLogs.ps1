00e269e52f4292a7fe9b61549dbcd2ec1c9f6410
Raxizzz <xesickytff@gmail.com>
2025-01-31 15:58:27 +0500

    Update EventLogs.ps1

--- EventLogs.ps1
+++ EventLogs.ps1
@@ -9,11 +9,8 @@ function Check-EventLog {
     }
 }
 
-# Check if the script is running as Administrator
 $runAsAdmin = [System.Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544'
-
 if (-not $runAsAdmin) {
-    # Relaunch the script with elevated privileges (Administrator)
     $argList = "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`""
     Start-Process powershell.exe -ArgumentList $argList -Verb RunAs
     Exit
@@ -25,5 +22,4 @@ Check-EventLog "System" 1074 "User recent PC Shutdown"
 Check-EventLog "Security" 4616 "System time changed"
 Check-EventLog "System" 6005 "Event Log Service started"
 
-# Add this line to pause the script before the window closes
 Read-Host -Prompt "Press Enter to exit"
